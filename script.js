abb801c33526b5fcedba883bb2c7bb7db0f318e7
Sakibul-05 <110473516+Sakibul-05@users.noreply.github.com>
2023-06-28 15:26:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+let text = document.getElementById("text");
+let delay = document.getElementById("delay");
+let submit = document.getElementById("btn");
+let output = document.getElementById("output");
+async function showMessage(){
+	// await return new Promise((resolve,reject)=>{
+	// 	if(text.value){
+	// 		setTimeout(()=>{
+	// 			resolve(text.value);
+	// 		},delay*1000)
+	// 	}
+	// })
+
+	setTimeout(()=>{
+		output.innerText = text.value;
+	},delay.value*1000)
+}
+ 
+submit.addEventListener("click", showMessage);
